a6b08f025bc146364f58d1e29e0be4f748812450
Vinc370 <84572867+Vinc370@users.noreply.github.com>
2023-08-22 16:37:50 +0700

    Entity

---
+++ src/main/java/com/gdp/codetest/Entity/User.java
@@ -0,0 +1,55 @@
+package com.gdp.codetest.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_m_user")
+public class User {
+    
+    @Id
+    private int user_id;
+    private String fullName;
+    private String dateOfBirth;
+    private String address;
+    private String phoneNumber;
+    private String gender;
+    
+    public int getUser_id() {
+        return user_id;
+    }
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
